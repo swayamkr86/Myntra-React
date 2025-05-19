709ff89fb48335f44de70c072ed4c83cc72d594c
swayamkr86 <137387677+swayamkr86@users.noreply.github.com>
2025-05-20 01:11:27 +0530

    Update Fetchitems.jsx

--- frontend/src/components/Fetchitems.jsx
+++ frontend/src/components/Fetchitems.jsx
@@ -14,7 +14,7 @@ const FetchItems = () => {
     const signal = controller.signal;
 
     dispatch(fetchStatusActions.markFetchingStarted());
-    fetch("http://localhost:8080/items", { signal })
+    fetch("https://myntra-react-backend.onrender.com/items", { signal })
       .then((res) => res.json())
       .then(({ items }) => {
         dispatch(fetchStatusActions.markFetchDone());
